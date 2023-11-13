14aa6a35a4535cf34dfa0e34fa026194dc5c7a62
MudBoxCode <mehdimosavi.shalhe@gmail.com>
2023-11-13 08:14:21 +0100

    Card Instantiate

--- CardBase.cs
+++ CardBase.cs
@@ -1,8 +1,12 @@
 using Godot;
 using System;
 
-public partial class CardBase : Node
+public partial class CardBase : Control
 {
+    private Label cardName;
+    private Label labelFireCost;
+    private Label labelWaterCost;
+    private Label labelEarthCost;
 
     public int FireCost {get; set;}
     public int WaterCost{get; set;}
@@ -21,10 +25,32 @@ public partial class CardBase : Node
         Description = description; 
     }
 
-   /* public void Effect()
+    public override void _Ready()
+    {
+        cardName = GetNode<Label>("Name");
+        labelFireCost = GetNode<Label>("FireCost");
+        labelWaterCost = GetNode<Label>("WaterCost");
+        labelEarthCost = GetNode<Label>("EarthCost");
+
+         if (cardName != null)
+            cardName.Text = $"Name: {Name}";
+
+        if (labelFireCost != null)
+            labelFireCost.Text = $"Fire Cost: {FireCost}";
+
+        if (labelWaterCost != null)
+            labelWaterCost.Text = $"Water Cost: {WaterCost}";
+
+        if (labelEarthCost != null)
+            labelEarthCost.Text = $"Earth Cost: {EarthCost}";
+
+ 
+    }
+
+    public void Effect()
     {
         GD.Print("Card Effect");
-    }*/
+    }
 
    
 
--- Hand.cs
+++ Hand.cs
@@ -4,26 +4,21 @@ using System.Collections.Generic;
 
 public partial class Hand : MarginContainer
 {
-	
+
+	PackedScene cardBaseScene = (PackedScene)ResourceLoader.Load("res://Card.tscn");
 
 	List<CardBase> deck = new List<CardBase>();
 	List<CardBase> cardsInHand = new List<CardBase>();
 
-	CardBase fireball = new CardBase(1, 0, 0, "Fireball", "Deal 5 damage");
+	/*CardBase fireball = new CardBase(1, 0, 0, "Fireball", "Deal 5 damage");
 	CardBase waterball = new CardBase(0, 1, 0, "Waterball", "Deal 5 damage");
 	CardBase earthball = new CardBase(0, 0, 1, "Earthball", "Deal 5 damage");
 	CardBase fireblast = new CardBase(2, 0, 0, "Fireblast", "Deal 10 damage");
 	CardBase waterblast = new CardBase(0, 2, 0, "Waterblast", "Deal 10 damage");	
 	CardBase earthblast = new CardBase(0, 0, 2, "Earthblast", "Deal 10 damage");
-	CardBase firestorm = new CardBase(3, 0, 0, "Firestorm", "Deal 15 damage");
+	CardBase firestorm = new CardBase(3, 0, 0, "Firestorm", "Deal 15 damage");*/
 
 
-
-	
-
-	
-
-	
 
 	[Export]
 	int maxHandSize = 5;
@@ -32,13 +27,18 @@ public partial class Hand : MarginContainer
 	public override void _Ready()
 	{
 		CardBase cardBase = GetNode<CardBase>("CardBase");
-		deck.Add(fireball);
-		deck.Add(waterball);
-		deck.Add(earthball);
-		deck.Add(fireblast);
-		deck.Add(waterblast);
-		deck.Add(earthblast);
-		deck.Add(firestorm);
+		deck.Add(new CardBase(1, 0, 0, "Fireball", "Deal 5 damage"));
+		deck.Add(new CardBase(0, 1, 0, "Waterball", "Deal 5 damage"));
+		deck.Add(new CardBase(0, 0, 1, "Earthball", "Deal 5 damage"));
+		deck.Add(new CardBase(2, 0, 0, "Fireblast", "Deal 10 damage"));
+		deck.Add(new CardBase(0, 2, 0, "Waterblast", "Deal 10 damage"));
+		deck.Add(new CardBase(0, 0, 2, "Earthblast", "Deal 10 damage"));
+		deck.Add(new CardBase(3, 0, 0, "Firestorm", "Deal 15 damage"));
+
+		/*for(int i = 0; i < maxHandSize; i++)
+		{
+			Draw();
+		}*/
 
 	}
 
@@ -54,13 +54,18 @@ public partial class Hand : MarginContainer
 	
 	public void Draw()
 	{
-		
+		Node cardBase = cardBaseScene.Instantiate();
+		AddChild(cardBase);
+
 		int randomIndex = new Random().Next(deck.Count);
 		CardBase drawnCard = deck[randomIndex];
 		cardsInHand.Add(drawnCard);
 		deck.RemoveAt(randomIndex);
 		
+		
 	}
+
+
 	
 		
 	
