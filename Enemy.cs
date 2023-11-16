9830a9fd859a35a39b315f77e2e662b79472e81f
BrutalFrost <hampuser@net.chalmers.se>
2023-11-16 11:21:09 +0100

    Added comments and made the characters into scenes instead of nodes

--- Enemy.cs
+++ Enemy.cs
@@ -7,25 +7,21 @@ public partial class Enemy : Actor
 {
 	// Called when the node enters the scene tree for the first time.
 	Player player;
-
+	
     public override void _Ready()
 	{
-		 player = GetNode<Player>("Player");
-		 queueOrder = 0;
 		 calculateAV();
-		 labelHP = GetNode<Label>("EnemyHp");
-		 displayHP();
+		 
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	
+	// To be renamed, take damage method
 	public override void _Attack(int dmg) {
 		hp -= dmg;
 		GD.Print("Player has attacked");
 		GD.Print("Current monster hp: "+hp);
-		displayHP();
 	}
-	public override void takeTurn (Actor target) {
+	public override void TakeTurn (Actor target) {
 		Thread.Sleep(500);
 		target._Attack(attack);
 		actionGauge = 10000;
